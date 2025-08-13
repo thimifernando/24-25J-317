172859b44899be3bc79ddf55180fe423fc0ffa7f
Galagamage Padumi Keshala <padumikeshala2000@gmail.com>
2025-08-13 23:20:04 +0530

    clear

--- Backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-DB_NAME = "flutter_auth"
-JWT_SECRET = os.getenv("JWT_SECRET", "your_jwt_secret")
